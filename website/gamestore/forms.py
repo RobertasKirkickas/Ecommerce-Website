7494145553a450962f8c9ecb1faf280cd8f24488
RobertasKirkickas <robertas3000@hotmail.com>
2025-02-06 20:43:02 +0000

    updated

--- website/gamestore/forms.py
+++ website/gamestore/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
     name = forms.CharField(max_length=100)
@@ -7,3 +8,23 @@ class ContactForm(forms.Form):
 
     def send_email(self):
         print(f"Sending email from {self.cleaned_data['email']} with message: {self.cleaned_data['message']}")
+
+
+class RegisterForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput, label='Confirm Password')
+
+    
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm']
+
+        def clean(self):
+            cleaned_data = super().clean()
+            password = cleaned_data.get('password')
+            password_confirm = cleaned_data.get('password_confirm')
+
+            # Check if passwords match
+            if password and password_confirm and password != password_confirm:
+                raise forms.ValidationError("Passwords do not match!")
+            return cleaned_data
