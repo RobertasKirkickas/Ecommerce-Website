b46827cc611e315f0519311fb0aa639dfa6b40fa
RobertasKirkickas <robertas3000@hotmail.com>
2025-02-10 16:25:48 +0000

    models

--- website/gamestore/admin.py
+++ website/gamestore/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
-from .models import Games
+from .models import Games, Item, Order, OrderItem
 # Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+       'title',
+       'price',
+       'discount_price'
+    ]
+
 admin.site.register(Games)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderItem)
