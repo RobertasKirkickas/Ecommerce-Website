2638373b20de3c2161720bca250035a2550dc9af
RobertasKirkickas <robertas3000@hotmail.com>
2025-03-05 13:30:39 +0000

    testing for live

--- website/website/urls.py
+++ website/website/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('accounts/register/', views.register_view, name='register'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
